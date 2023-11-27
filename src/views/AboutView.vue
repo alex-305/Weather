99148ac827eb22667ba69932f1a8cd74a932a7b3
Alex Gonzalez <galex2003@icloud.com>
2023-11-27 16:22:52 -0500

    Updated about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,8 @@
 
 <div>
     <h2>About</h2>
-    <p>This is a weather app</p>
+    <p>This is a weather app that I created in my spare time
+        to learn how to use APIs in my web applications.</p>
 </div>
 
 </template>
